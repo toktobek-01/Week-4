1f0039237d90fd7cf663c9610a35ce77f26b1b81
toktobek-01 <toktobek.d@gmail.com>
2025-05-01 08:14:17 -0500

    dog

---
+++ src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+ public String name;
+
+ public Cat(String name){
+     this.name = name;
+ }
+
+ public void myau() {
+     System.out.println(this.name + " myau");
+ }
+}
---
+++ src/mainCat.java
@@ -0,0 +1,9 @@
+public class mainCat {
+    public static void main(String[] args) {
+  Cat Puppy = new Cat("Puppy");
+  Puppy.myau();
+
+  Cat Shaffy = new Cat("Shaffy");
+  Shaffy.myau();
+    }
+}
