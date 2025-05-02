2fd8b9398b6b520d64cc9541351b5cd2b9770e9f
toktobek-01 <toktobek.d@gmail.com>
2025-05-01 22:20:27 -0500

    dog

---
+++ src/catOne.java
@@ -0,0 +1,5 @@
+public class catOne {
+    public void meow() {
+        System.out.println("The cat is mewoing!");
+    }
+}
---
+++ src/mainCatOne.java
@@ -0,0 +1,6 @@
+public class mainCatOne {
+    public static void main(String[] args) {
+        catOne c = new catOne();
+        c.meow();
+    }
+}
